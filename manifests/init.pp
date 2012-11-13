dbd5f5000382e622c8e252d67a7dcbd3e937d348
Micah Anderson <micah@riseup.net>
2012-11-13 12:12:16 -0500

    setup parameterized class to enable versioned installation of ruby package on different operating system

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,37 @@
-class ruby {
-  package{'ruby': 
-    ensure => installed, 
+# Class: ruby
+#
+# This class installs Ruby
+#
+# Parameters:
+#
+# version: (default installed)
+# Set the version of Ruby to install
+#
+# Sample Usage:
+#
+# For a standard install using the latest ruby, simply do:
+#
+# class { 'ruby': }
+#
+# On Debian this is equivilant to
+# $ apt-get install ruby
+#
+# To install a specific version of ruby, simply do:
+#
+# class { 'ruby':
+#    version => '1.8.7',
+# }
+#
+# Supported version: 1.8, 1.8.7, 1.9, 1.9.1, 1.9.3
+
+class ruby (
+  $version      = $ruby::params::version,
+  $ruby_dev     = $ruby::params::ruby_dev,
+  $ruby_package = $ruby::params::ruby_package
+) inherits ruby::params {
+
+  package{ 'ruby':
+    ensure => $version,
+    name   => $ruby_package
   }
 }
