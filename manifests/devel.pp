9ccd853c49af7d0b57ebd9c2ea7673b193fce24b
varac <varacanero@zeromail.org>
2016-01-22 23:50:27 +0100

    [feat] switch to ensure_packages() for ruby::devel

--- manifests/devel.pp
+++ manifests/devel.pp
@@ -1,8 +1,5 @@
+# install ruby header files and rake
 class ruby::devel {
   include ruby
-
-  package { $ruby::ruby_dev:
-    ensure  => installed,
-    require => Package['ruby'],
-  }
+  ensure_packages($ruby::ruby_dev)
 }
